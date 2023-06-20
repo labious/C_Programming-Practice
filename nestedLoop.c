b1b762e0eccc2131d3730fb4f6864a1b7063bf50
labious <labiousphetoane@gmail.com>
2023-06-20 12:45:35 -0700

    nestedLoop.c

--- nestedLoop.c
+++ nestedLoop.c
@@ -5,7 +5,30 @@
  *
  * Return: Always 0 (Success)
  */
+
+/* function declaration */ 
+int max(int num1,int num2);
+
 int main(void)
+{	
+	int a = 100;
+	int b = 350;
+	int ret;
+	/* function call */
+	ret = max(a,b);
+
+	printf("Max value is  : %d\n", ret );
+	return (0);
+}
+
+/*function returning max betwixt 2 numbers*/
+int max(int num1, int num2)
 {
-    return (0);
+	/*Local variable*/
+	int result;
+	if(num1 > num2)
+		result = num1;
+	else
+		result = num2;
+	return result;
 }
